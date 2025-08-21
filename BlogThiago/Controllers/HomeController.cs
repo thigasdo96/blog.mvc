795b0d227ece798c40235c7d1c3a3c1de19fa5d9
thigasdo96 <primothiago8@gmail.com>
2025-08-20 22:22:05 -0300

    Criando pagina postagem

--- BlogThiago/Controllers/HomeController.cs
+++ BlogThiago/Controllers/HomeController.cs
@@ -8,14 +8,12 @@ namespace BlogThiago.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private List<Categoria> categorias;
+    private List<Postagem> postagens;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
         Categoria tecnologia = new();
         tecnologia.Id = 1;
         tecnologia.Nome = "Tecnologia";
@@ -31,7 +29,7 @@ public class HomeController : Controller
             Nome = "NARUTO"
         };
 
-        List<Postagem> postagens = [
+        postagens = [
             new() {
                 Id = 1,
                 Nome = "Saiu o ChatGPT 5",
@@ -53,12 +51,34 @@ public class HomeController : Controller
                 Texto = "akdjawodkjas",
                 Thumbnail = "/img/paula.jpg",
                 Foto = "/img/paula.jpg"
-            }
+            },
+            new() {
+                Id = 3,
+                Nome = "Naruto é muito bom",
+                CategoriaId = 4,
+                Categoria = naruto,
+                DataPostagem = DateTime.Parse("08/08/2025"),
+                Descricao = "Naruto Clássico, também conhecido como a primeira parte do anime Naruto, refere-se aos primeiros 220 episódios da série, que contam a história de Naruto Uzumaki durante sua infância e pré-adolescência. Essa fase foca no desenvolvimento do personagem, suas relações com outros ninjas e a busca pelo reconhecimento como Hokage. ",
+                Texto = "akdjawodkjas",
+                Thumbnail = "/img/li.jpg",
+                Foto = "/img/li.jpg"
+            },
+            
         ];
+    }
+
+    public IActionResult Index()
+    {
+        
         return View(postagens);
     }
     public IActionResult Postagem(int id)
     {
+        var postagem = postagens
+            .Where(p => p.Id == id)
+            .SingleOrDefault();
+        if (postagem == null)
+            return NotFound();
         return View();
     }
     public IActionResult Privacy()
